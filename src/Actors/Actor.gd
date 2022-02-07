ed28a2fd59900fdd5be83f8bd24df6cd07595f9b
Pedro Sousa <pedrosousa2708@gmail.com>
2022-02-07 12:07:32 -0300

    New Player sprite; New animations;

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 class_name Actor
 
+enum State { IDLE = 0, JUMP, RUN, ROLL, ATTACK, DEFEND, DEATH }
+
 export var speed := Vector2(300.0, 1000)
 export var gravity := 3000.0
-var velocity := Vector2.ZERO
 
-func _physics_process(delta: float) -> void: 
-	velocity.y += gravity * delta
-	# velocity.y = max(speed.y, velocity.y)
+var velocity := Vector2.ZERO
+var state: int = State.IDLE
--- src/Actors/Player/Player.gd
+++ src/Actors/Player/Player.gd
@@ -1,16 +1,80 @@
 extends Actor
+class_name Player
 
-onready var _animationPlayer = $AnimationPlayer
+const idle: String = "idle"
+const jump: String = "jump"
+const attack: String = "attack"
+const defend: String = "defend"
+const death: String = "death"
+const roll: String = "roll"
+const run: String = "run"
 
-func _physics_process(delta) -> void:
-	_animationPlayer.play("idle")
+var currentAnimationName: String = idle
+onready var _animation = $AnimationPlayer
+onready var _sprite = $spriteNode
+onready var _collision = $CollisionShape2D
 
-	var direction := Vector2(
+func _ready():
+	_animation.play("idle")
+
+func _input(event):
+	if  !is_on_floor():
+		return
+
+	if event.is_action_pressed(jump):
+		changeAnimation(jump)
+		state = State.JUMP
+	elif event.get_action_strength("moveLeft") > 0:
+		changeAnimation(run)
+		state = State.RUN
+		if _sprite.scale.x > 0:
+			_flipScale()
+	elif event.get_action_strength("moveRight") > 0:
+		changeAnimation(run)
+		state = State.RUN
+		if _sprite.scale.x < 0:
+			_flipScale()
+	elif event.is_action_pressed(roll):
+		changeAnimation(roll)
+		state = State.ROLL
+	elif event.get_action_strength("attack") > 0:
+		changeAnimation(attack)
+		state = State.ATTACK
+	elif event.get_action_strength("defend") > 0:
+		changeAnimation(defend)
+		state = State.DEFEND
+	else:
+		changeAnimation(idle)
+		state = State.IDLE
+
+func _flipScale():
+	_sprite.scale.x *= -1
+	_collision.scale.x *= -1
+
+func _physics_process(delta: float) -> void:
+	var direction := getDirection()
+	velocity = getVelocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	return
+
+func getDirection() -> Vector2:
+	return Vector2(
 		Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft"),
-		1.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
 
-	velocity = speed * direction
-	velocity = move_and_slide(velocity)
-	
-	return
+func getVelocity(linearVelocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
+	var newVelocity = linearVelocity
+	newVelocity.x = speed.x * direction.x
+	newVelocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		newVelocity.y = speed.y * direction.y
+	return newVelocity
+
+func changeAnimation(animationName: String):
+	if animationName == currentAnimationName:
+		return
+
+	_animation.play(animationName)
+	currentAnimationName = animationName
