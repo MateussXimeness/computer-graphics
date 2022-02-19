0e641429e7964680941e65655d1a70ee1202605d
Hiago <hiago.chagas.14@gmail.com>
2022-02-19 10:44:16 -0300

    Added newStart as initial scene

--- src/Scenes/NewStart/NewStart.gd
+++ src/Scenes/NewStart/NewStart.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 
 func _on_START_pressed():
-	get_tree().change_scene("res://src/Scenes/InitialScene/InitialScene.tscn")
+	get_tree().change_scene("res://src/Scenes/Level1/Mundo.tscn")
 
 
 func _on_QUIT_pressed():
